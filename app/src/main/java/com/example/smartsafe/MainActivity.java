c1bead3bdd3a09d373eab014a266fbc2c0b6ff44
hzz86 <guswn_781@naver.com>
2019-05-09 18:06:26 +0900

    Test

--- app/src/main/java/com/example/smartsafe/MainActivity.java
+++ app/src/main/java/com/example/smartsafe/MainActivity.java
@@ -7,6 +7,8 @@ public class MainActivity extends AppCompatActivity {
 
     //주효가 수정
     //주효가 또 수정
+    //현주가 수정
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
