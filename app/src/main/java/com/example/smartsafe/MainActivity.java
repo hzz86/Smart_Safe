3dbb18c9c55b4b4422ac764889c0ff5bfd947914
kjuhyo <dkrlwngy@gmail.com>
2019-05-10 18:07:03 +0900

    주효가 또 수정

--- app/src/main/java/com/example/smartsafe/MainActivity.java
+++ app/src/main/java/com/example/smartsafe/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     //주효가 수정
+    //주효가 또 수정
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
