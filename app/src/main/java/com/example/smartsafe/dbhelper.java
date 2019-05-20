1bcb29392bd429c20b1ad805ac10831e73a2368b
kjuhyo <dkrlwngy@gmail.com>
2019-05-20 17:25:11 +0900

    dbhelper.java 생성

---
+++ app/src/main/java/com/example/smartsafe/dbhelper.java
@@ -0,0 +1,28 @@
+package com.example.smartsafe;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+class dbhelper extends SQLiteOpenHelper {
+    private static final String DB_name="ourdb1.db";
+    private static final int DB_version=1;
+
+    public dbhelper(Context context, String DB_name, SQLiteDatabase.CursorFactory factory, int DB_version) {
+        super(context, DB_name, factory, DB_version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table Member(id INTEGER PRIMARY KEY AUTOINCREMENT,name text, mobile number, email text)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS Member");
+        onCreate(db);
+    }
+
+}
+
